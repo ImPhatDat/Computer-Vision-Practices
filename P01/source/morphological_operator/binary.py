22b9295f28caae3f11073a9d3604be838857b10e
ImPhatDat <phatdatn2003@gmail.com>
2024-07-09 15:20:38 +0700

    reupload binary.py

---
+++ P01/source/morphological_operator/binary.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def erode(img, kernel):
+    kernel_center = (kernel.shape[0] // 2, kernel.shape[1] // 2)
+    kernel_ones_count = kernel.sum()
+    eroded_img = np.zeros((img.shape[0] + kernel.shape[0] - 1, img.shape[1] + kernel.shape[1] - 1))
+    img_shape = img.shape
+
+    x_append = np.zeros((img.shape[0], kernel.shape[1] - 1))
+    img = np.append(img, x_append, axis=1)
+
+    y_append = np.zeros((kernel.shape[0] - 1, img.shape[1]))
+    img = np.append(img, y_append, axis=0)
+
+    for i in range(img_shape[0]):
+        for j in range(img_shape[1]):
+            i_ = i + kernel.shape[0]
+            j_ = j + kernel.shape[1]
+            if kernel_ones_count == (kernel * img[i:i_, j:j_]).sum() / 255:
+                eroded_img[i + kernel_center[0], j + kernel_center[1]] = 1
+
+    return eroded_img[:img_shape[0], :img_shape[1]]
+
+
+'''
+TODO: implement morphological operators
+'''
+# redefine
+def erode(img, kernel):
+    kernel_center = (kernel.shape[0] // 2, kernel.shape[1] // 2)
+    kernel_ones_count = kernel.sum()
+    padded_img = np.pad(img, ((kernel_center[0], kernel_center[0]), (kernel_center[1], kernel_center[1])), 
+                        mode='constant', constant_values=0) # use pad instead of append
+    eroded_img = np.zeros_like(img)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            region = padded_img[i:i + kernel.shape[0], j:j + kernel.shape[1]]
+            if np.sum(region * kernel) == kernel_ones_count * 255:
+                eroded_img[i, j] = 255 # use 255 instead of 1
+
+    return eroded_img
+
+
+def dilate(img, kernel):
+    kernel_center = (kernel.shape[0] // 2, kernel.shape[1] // 2)
+    padded_img = np.pad(img, ((kernel_center[0], kernel_center[0]), (kernel_center[1], kernel_center[1])), 
+                        mode='constant', constant_values=0) # use pad instead of append
+    eroded_img = np.zeros_like(img)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            region = padded_img[i:i + kernel.shape[0], j:j + kernel.shape[1]]
+            if np.any(region == kernel):
+                eroded_img[i, j] = 255 # use 255 instead of 1
+
+    return eroded_img
+
+def opening(img, kernel):
+    return dilate(erode(img, kernel), kernel)
+
+def closing(img, kernel):
+    return erode(dilate(img, kernel), kernel)
+
+def hitmiss(img, kernel):
+    return erode(dilate(img, kernel), kernel)
